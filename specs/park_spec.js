dce3b16b6c21c05f881280e0b6ffbd34bee36c29
James Cox <j-cox03@hotmail.com>
2017-07-25 21:35:09 +0100

    Made files for Park and Park tests and made basic outline for Park tests

---
+++ specs/park_spec.js
@@ -0,0 +1,12 @@
+var assert = require("assert");
+var Park = require("../park.js");
+var Dinosaur = require("../dinosaur.js");
+
+describe("park", function(){
+  var park;
+
+  beforeEach(function(){
+    park = new Park();
+  });
+
+});
