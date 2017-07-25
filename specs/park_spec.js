4f21e0e0a7c0278301d8ffe6b2f01445f58df4a8
James Cox <j-cox03@hotmail.com>
2017-07-25 22:02:33 +0100

    park_spec: moved dinos array creation to beforeEach method

--- specs/park_spec.js
+++ specs/park_spec.js
@@ -4,29 +4,27 @@ var Dinosaur = require("../dinosaur.js");
 
 describe("park", function(){
   var park;
+  var dinos;
 
   beforeEach(function(){
     park = new Park();
+    dinos = [];
+    dinos[0] = new Dinosaur("Tyrannosaurus", 2);
+    dinos[1] = new Dinosaur("Tyrannosaurus", 2);
+    dinos[2] = new Dinosaur("Velociraptor", 1);
   });
 
   it("can add dinosaurs to enclosure", function() {
-    dinos = [];
-    dinos[0] = new Dinosaur("Tyrannosaurus", 2);
-    dinos[1] = new Dinosaur("Velociraptor", 1);
+    for(var dino of dinos){
+      park.add(dino);
+    }
 
-    park.add(dinos[0]);
-    park.add(dinos[1]);
     for(var index in dinos){
       assert.strictEqual(park.enclosure[0], dinos[0]);
     }
   });
 
   it("can remove dinosaurs of given type", function() {
-    dinos = [];
-    dinos[0] = new Dinosaur("Tyrannosaurus", 2);
-    dinos[1] = new Dinosaur("Tyrannosaurus", 2);
-    dinos[2] = new Dinosaur("Velociraptor", 1);
-
     for(var dino of dinos){
       park.add(dino);
     }
