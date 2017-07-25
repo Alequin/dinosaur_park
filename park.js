06367e5e56edaac8bf67934966d413ad07a3a38d
James Cox <j-cox03@hotmail.com>
2017-07-25 21:57:47 +0100

    park: made remove method

--- park.js
+++ park.js
@@ -7,4 +7,13 @@ Park.prototype.add = function(dinosaur){
   this.enclosure.push(dinosaur);
 }
 
+Park.prototype.remove = function(type){
+  var length = this.enclosure.length;
+  for(var j=length-1; j>=0; j--){
+    if(this.enclosure[j].type = type){
+      this.enclosure.splice(j, 1);
+    }
+  }
+}
+
 module.exports = Park;
