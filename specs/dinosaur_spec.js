5fbdf48a1fc665441e2bb7452353fb863cb97fdf
James Cox <j-cox03@hotmail.com>
2017-07-25 21:32:14 +0100

    Made basic tests for construction of a dinosaur

--- specs/dinosaur_spec.js
+++ specs/dinosaur_spec.js
@@ -8,4 +8,12 @@ describe("dinosaur", function(){
     dino1 = new Dinosaur("Tyrannosaurus", 2);
   });
 
+  it("should have a type on creation", function(){
+    assert.strictEqual(dino1.type, "Tyrannosaurus");
+  });
+
+  it("should have an offspringPerYear count on creation", function(){
+    assert.strictEqual(dino1.offspringPerYear, 2);
+  });
+
 });
