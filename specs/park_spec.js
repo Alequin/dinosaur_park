ddf4ede62b4436d9aed66e494c0671ce32de25e2
James Cox <j-cox03@hotmail.com>
2017-07-25 21:40:12 +0100

    park_specs: made test for adding dinosaurs to park enclosure

--- specs/park_spec.js
+++ specs/park_spec.js
@@ -9,4 +9,16 @@ describe("park", function(){
     park = new Park();
   });
 
+  it("can have dinosaurs added to it", function() {
+    dinos = [];
+    dinos[0] = new Dinosaur("Tyrannosaurus", 2);
+    dinos[1] = new Dinosaur("Velociraptor", 1);
+
+    park.add(dinos[0]);
+    park.add(dinos[1]);
+    for(var index in dinos){
+      assert.strictEqual(park.enclosure[0], dinos[0]);
+    }
+  });
+
 });
