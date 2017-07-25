3eead1fa2bafa7371ff49bb455034d900a3260ed
James Cox <j-cox03@hotmail.com>
2017-07-25 21:25:53 +0100

    Made dinosaur.js and dinosaur_spec.js

---
+++ specs/dinosaur_spec.js
@@ -0,0 +1,11 @@
+var assert = require("assert");
+var Dinosaur = require("../dinosaur.js");
+
+describe("dinosaur", function(){
+  var dino1;
+
+  beforeEach(function(){
+    dino1 = new Dinosaur("Tyrannosaurus", 2);
+  });
+
+});
